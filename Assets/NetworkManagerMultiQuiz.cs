f6db3c00887e9b8be3d7078d51b2710c8fa6d03f
Martin 'quill18' Glaude <quill18@quill18.com>
2019-06-30 15:19:45 -0400

    Setting Up Scenes

---
+++ Assets/NetworkManagerMultiQuiz.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerMultiQuiz : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Debug.Log("OnServerAddPlayer");
+        base.OnServerAddPlayer(conn, playerControllerId);
+    }
+
+}
